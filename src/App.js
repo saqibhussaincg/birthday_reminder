5d8f021cba4b391bb08a57b1d504d504087d9fe0
saqibhussaincg <saqibhussaincg@gmail.com>
2022-08-21 20:09:45 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
+import List from './List';
+import { useState } from 'react';
 
 function App() {
+  const [people, setProple] = useState(data);
+  // console.log(people);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+        <section className='container'>
+          <h2>Birthday Reminder</h2>
+          <h3>{people.length} Birthday Today</h3>
+          {/* <h2>{people[1].name}</h2> */}
+          <List birthdaypeople={people} />
+        <button onClick={() => console.log('you clicked me!')}>Clear All</button>
+        </section>
+      </main>
   );
 }
 
